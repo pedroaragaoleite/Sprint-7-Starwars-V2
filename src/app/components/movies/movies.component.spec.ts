2565f909c8d059e35052eb16fc13338f4a25cdbd
Pedro Aragão <pedro.aragao.leite@gmail.com>
2024-03-01 11:08:06 +0100

    movies componet test

--- src/app/components/movies/movies.component.spec.ts
+++ src/app/components/movies/movies.component.spec.ts
@@ -1,17 +1,32 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MoviesComponent } from './movies.component';
+import { ApiStarwarsService } from '../../services/api-starwars.service';
+import { BehaviorSubject } from 'rxjs';
+BehaviorSubject
+ApiStarwarsService
 
 describe('MoviesComponent', () => {
   let component: MoviesComponent;
   let fixture: ComponentFixture<MoviesComponent>;
+  let apiStarwarsServiceMock: any;
 
   beforeEach(async () => {
+    const filmsUrls = new BehaviorSubject<string[]>(['/api/films/1']);
+    const moviesNames = new BehaviorSubject<string[]>(['A New Hope']);
+
+    apiStarwarsServiceMock = {
+      currentFilmUrl: filmsUrls.asObservable(),
+      currentMovieNameUrl: moviesNames.asObservable()
+    };
+
+
     await TestBed.configureTestingModule({
-      imports: [MoviesComponent]
+      imports: [MoviesComponent],
+      providers: [{ provide: ApiStarwarsService, useValue: apiStarwarsServiceMock }]
     })
-    .compileComponents();
-    
+      .compileComponents();
+
     fixture = TestBed.createComponent(MoviesComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -19,5 +34,19 @@ describe('MoviesComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+  it('should update filmsArray and movieNames from the service on init', () => {
+    expect(component.filmsArray).toEqual(['/api/films/1']);
+    expect(component.movieNames).toEqual(['A New Hope']);
+  });
+
+  it('should update filmsArray and movieNames when service emits new values', () => {
+    // Emit new values
+    apiStarwarsServiceMock.currentFilmUrl.next(['/api/films/2']);
+    apiStarwarsServiceMock.currentMovieNameUrl.next(['The Empire Strikes Back']);
+
+    // Assert updated values
+    expect(component.filmsArray).toEqual(['/api/films/2']);
+    expect(component.movieNames).toEqual(['The Empire Strikes Back']);
   });
 });
